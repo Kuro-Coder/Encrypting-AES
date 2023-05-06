4462c3ca0f79ff7a28e8bde6699b6278c1f30e72
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-05-06 20:18:41 +0330

    add all my text and...

--- Encrypting-AES/Program.cs
+++ Encrypting-AES/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Yo yo, wellcome to string Encrypting App.");
+
+Console.WriteLine("Type your string data:");
+
+
+
+Console.ReadKey();
