9b6a9f9d212faa412a2df498d336d5ee42853510
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-05-06 20:19:56 +0330

    create my method

--- Encrypting-AES/Program.cs
+++ Encrypting-AES/Program.cs
@@ -1,10 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Yo yo, wellcome to string Encrypting App.");
 
 Console.WriteLine("Type your string data:");
 
+var cryptoData = Encrypting(Console.ReadLine());
 
+Console.WriteLine(cryptoData);
 
 Console.ReadKey();
+
+
+static string Encrypting(string plainText)
+{
+
+
+    string cipherText = "";
+    return cipherText;
+}
