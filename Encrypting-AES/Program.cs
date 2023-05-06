b8cdb3847fd5288906435a748b66509e5e26a48e
Habib PourKhnLar Ahmadi <habib.pa98@gmail.com>
2023-05-06 20:25:31 +0330

    last change to close app, enjoy.

--- Encrypting-AES/Program.cs
+++ Encrypting-AES/Program.cs
@@ -4,11 +4,14 @@ using System.Security.Cryptography;
 
 Console.WriteLine("Yo yo, wellcome to string Encrypting App.");
 
-Console.WriteLine("Type your string data:");
+Console.Write("Type your string data:");
 
 var cryptoData = Encrypting(Console.ReadLine());
 
+Console.Write("Encrypting Data: ");
 Console.WriteLine(cryptoData);
+
+Console.WriteLine("Finish Encrypting, press any key pls.");
 
 Console.ReadKey();
 
@@ -21,15 +24,11 @@ static string Encrypting(string plainText)
     using (AesManaged aes = new())
     {
         byte[] IV = new byte[16];
-        byte[] Key = {
-                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-                    0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16
-                };
+        byte[] Key = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
 
         var mode = aes.Mode;//aes.Mode = CipherMode.CBC;
-        ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);// Create encryptor    
-                                                                  // Create MemoryStream    
-        using (MemoryStream ms = new MemoryStream())
+        ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);// Create encryptor  
+        using (MemoryStream ms = new MemoryStream()) // Create MemoryStream   
         {
             // Create crypto stream using the CryptoStream class. This class is the key to encryption    
             // and encrypts and decrypts data from any given stream. In this case, we will pass a memory stream    
